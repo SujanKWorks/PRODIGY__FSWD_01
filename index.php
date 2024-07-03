9b6f742083aaba058c559f581323006b06d427ba
SujanKWorks <sujakarkada5@gmail.com>
2024-07-03 22:31:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SECURE</title>
+</head>
+<body>
+    <form class="login-form" action="" method="post">
+        Email<input type="text">
+        Password<input type="password">
+        <input type="submit">
+    </form>
+</body>
+</html>
