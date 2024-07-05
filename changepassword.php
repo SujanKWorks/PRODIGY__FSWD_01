1b8ad45c38dd2a658d6c697bbf8c82c24e22da3f
SujanKWorks <sujakarkada5@gmail.com>
2024-07-05 22:41:42 +0530

    Add files via upload

---
+++ changepassword.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Password</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+    <!-- Change Password page -->
+    <div class="change-password-page">
+        <div class="change-password-page-content">
+            <form action="changepassword.php" class="change-password-form" method="POST">
+                <input type="text" class="form-password-input" placeholder="Email">
+                <input type="text" class="form-password-input" placeholder="New Password">
+                <input type="submit" class="form-password-btn" value="Confirm">
+            </form>
+        </div>
+    </div>
+    <!-- End of Change Password page -->
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SECURE</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <!-- Login page -->
     <div class="login-page">
@@ -16,24 +18,33 @@
                 <input type="submit" class="form-login-btn" value="Log In">
                 <a href="#" class="form-login-link">Forgot Password?</a>
                 <span>or</span>
-            </form>     
-        <button class="form-signup-btn signup"  >signup</button>
+            </form>
+            <button class="form-signup-btn signup">signup</button>
+        </div>
     </div>
-</div>
     <!-- End of Login page -->
 
     <!-- Sign Up page -->
-<div class="signup-page">
-    <div class="signup-page-content">
-        <form action="signup.php" class="signup-page-form" method="POST" >
-            <input type="text" class="form-signup-input" name="username" placeholder="User Name" required>
-            <input type="text" class="form-signup-input" name="email" placeholder="Email" required>
-            <input type="password"class="form-signup-input"  name="password" placeholder="Password" required>
-            <input type="submit" class="signup-btn" value="Sign Up">
-        </form>
-    </div>
-</div>
-    <!--End of Sign Up page -->
+    <div class="signup-page">
+        <div class="signup-page-content">
+            <form action="signup.php" class="signup-page-form" method="POST">
+                <input type="text" class="form-signup-input" name="username" placeholder="User Name" required>
+                <input type="text" class="form-signup-input" name="email" placeholder="Email" required>
+                <input type="password" class="form-signup-input" name="password" placeholder="Password" required>
+                <input type="submit" class="signup-btn" value="Sign Up">
+                <span>Have an Account?</span>
+            </form>
+            <button class="form-signup-btn signup">Log In</button>
 
+        </div>
+    </div>
+    <!--End of Sign Up page -->
+    
+    <div class="profile-page">
+        <div class="profile-page-content">
+
+        </div>
+    </div>
 </body>
+
 </html>
